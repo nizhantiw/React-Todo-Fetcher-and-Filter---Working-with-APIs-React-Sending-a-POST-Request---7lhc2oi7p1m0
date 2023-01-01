0a959d11ffc8cbe9d23e4fa6a097608aaa97d9bd
nizhantiw <nizhantiw@gmail.com>
2023-01-01 19:49:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import '../styles/App.css';
-import { Loader } from './Loader';
 import { Todo } from './Todo';
+
 const App = () => {
   
-  
+  return (
+    <div className="App">
+      <Todo  />
+    </div>
+  );
 }
 
 
