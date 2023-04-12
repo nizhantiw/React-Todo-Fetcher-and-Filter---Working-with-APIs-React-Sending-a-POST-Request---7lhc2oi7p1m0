f1492e8e0639cc5da3dc1a03166e9fddc1247ef2
nizhantiw <nizhantiw@gmail.com>
2023-04-12 17:36:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,89 @@
-import React, { useEffect, useState } from 'react'
-import '../styles/App.css';
-import { Loader } from './Loader';
-import { Todo } from './Todo';
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
+import { Loader } from "./Loader";
+import { Todo } from "./Todo";
 
 const App = () => {
-    useEffect(() => { fetchTodo(); }, []);
-    const [todo, setTodo] = useState([]);
-    const [loader, setLoader] = useState(true);
-    const [completed, setCompleted] = useState(true);
-    const [incomplete, setIncomplete] = useState(true);
-    const fetchTodo = () => {
+  const [todo, setTodos] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [showData, setShowData] = useState([]);
+  const [checkedCompleted, setCheckedCompleted] = useState(true)
+  const [checkedInCompleted, setCheckedInCompleted] = useState(true)
 
-        fetch('https://jsonplaceholder.typicode.com/todos')
-            .then(response => response.json())
-            .then(data => {
-                setTodo(data.slice(0, 20));
-                setLoader(false);
-            });
+  useEffect(() => {
+    getAllTodos();
+  }, []);
+
+  useEffect(() => {
+    loadFilterProducts();
+  }, [checkedCompleted, checkedInCompleted]);
+
+
+  
+  const getAllTodos = async () => {
+    setLoader(true);
+    await fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => {
+        setTodos(data.slice(0,20));
+        setShowData(data.slice(0,20));
+      });
+    setLoader(false);
+  };
+
+  const loadFilterProducts = () => {
+    if(checkedCompleted && checkedInCompleted){
+      let newFilter = [...todo];
+      setShowData(newFilter);
+    } else if(checkedCompleted){
+      let newFilter = [...todo];
+      newFilter = newFilter.filter((data) => data.completed)
+      setShowData(newFilter);
+      setCheckedCompleted(true)
+    } else if(!checkedCompleted){
+      let newFilter = [...todo];
+      newFilter = newFilter.filter((data) => !data.completed)
+      setShowData(newFilter);
+      setCheckedInCompleted(true)
     }
-    return (
+  }
+  
+  return (
+    <>
+      {loader ? <Loader /> : null}
+      <ol>
+        {showData.map(
+          (t) =>
+              <li key={t.id}>
+                {" "}
+                <Todo id={t.id} title={t.title} completed={t.completed} />{" "}
+              </li>
+        )}
+      </ol>
 
-        <div>
+      <div id="filter-holder">
+        <label htmlFor="completed-checkbox">Show completed</label>
+        <input
+          type="checkbox"
+          id="completed-checkbox"
+          name="complete"
+          value="completed"
+          checked={checkedCompleted}
+          onChange={(e) => setCheckedCompleted(e.target.checked)}
+        />{" "}
+        <br />
+        <label htmlFor="incompleted-checkbox">Show Incompleted</label>
+        <input
+          type="checkbox"
+          id="incompleted-checkbox"
+          name="incomplete"
+          value="incompleted"
+          checked={checkedInCompleted}
+          onChange={(e) => setCheckedInCompleted(e.target.checked)}
+        />
+      </div>
+    </>
+  );
+};
 
-            <div id='filter-holder'>
-                <div>
-                    <input onChange={(e) => { setCompleted(e.target.checked) }} type="checkbox" checked={completed} id='completed-checkbox' /> Completed
-                </div>
-                <div>
-                    <input onChange={(e) => { setIncomplete(e.target.checked) }} type="checkbox" checked={incomplete} id='incompleted-checkbox' /> InCompleted
-                </div>
-            </div>
-            {loader ? <Loader /> : todo.map((e) => {
-                if (completed === e.completed || incomplete !== e.completed) {
-                    return <Todo key={e.id} id={e.id} title={e.title} completed={e.completed} />
-                }
-            }
-            )}
-        </div>
-    )
-}
 export default App;
